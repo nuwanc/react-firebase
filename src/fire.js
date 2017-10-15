03971233cf0646de8b8f6bd4d4f5e7f4e6baec42
Nuwan Chandrasoma <mymailnot@gmail.com>
2017-10-15 21:35:37 +0530

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 let config = {
-    apiKey: "AIzaSyAdolkiFfazXwM6a-jSPmhdYykUTBjaDpQ",
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     authDomain: "react-firebase-513a3.firebaseapp.com",
     databaseURL: "https://react-firebase-513a3.firebaseio.com",
     projectId: "react-firebase-513a3",
     storageBucket: "react-firebase-513a3.appspot.com",
-    messagingSenderId: "191875281765"
+    messagingSenderId: "xxxxxxxxxxxxxxx"
 };
 const fire = firebase.initializeApp(config);
 
